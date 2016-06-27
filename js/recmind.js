caaa7459a438738a0a6b915c0cd9478579435d37
Chao.Chen <ppiachen@gmail.com>
2016-06-27 17:19:04 +0800

    add a major js file

---
+++ js/recmind.js
@@ -0,0 +1,8 @@
+document.getElementById("navigation").innerHTML = "<div class='header'><div class='header_resize'><div class='menu_nav'>"
+		+ "<ul><li><a href='index.html'>Home</a></li>"
+		+ "<li class='active'><a href='pub.html'>Publications</a></li>"
+		+ "<li><a href='about.html'>About Us</a></li>"
+		+ "<div class='clr'></div></div>"
+		+ "<div class='clr'></div>"
+		+ "<div class='logo'><h1><a href='index.html'>Welcome to Recmind<br /><small>Keep Recommendation in Mind</small></a></h1></div>"
+		+ "</div></div>";
